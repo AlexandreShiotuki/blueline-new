d7fe5587e3ed24cb8c439c3b1e30862980e65a32
Alexandre Shiotuki <106483068+AlexandreShiotuki@users.noreply.github.com>
2023-10-27 14:43:05 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function disabled() {
-  if (true) {
-    alert("Material indisponível por enquanto")
-  }
-}
+window.onload = function() {
+    const popup = document.getElementById("popup");
+    const closeBtn = document.getElementById("close-btn");
+
+    popup.style.display = "block";
+
+    closeBtn.onclick = function() {
+        popup.style.display = "none";
+    };
+};
